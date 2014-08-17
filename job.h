3adcd8c7ff304c02608588c3f5a6797abd58cdc3
아띠 <duckhoim@naver.com>
2014-08-17 15:22:25 +0900

    Create job.h

---
+++ job.h
@@ -0,0 +1,18 @@
+char job(unsigned char code) {
+	switch(code) {
+	// 주인공
+	case 0: return "도적"; break;
+	case 1: return "전사"; break;
+	case 2: return "마법사"; break;
+	case 3: return "궁수"; break;
+	case 4: return "닌자"; break;
+	case 5: return "듀얼 블레이드"; break;
+	// 파트너
+	case 6: return "뮤턴트"; break;
+	case 7: return "뮤턴트"; break;
+	case 8: return "뮤턴트"; break;
+	case 9: return "뮤턴트"; break;
+	case 10: return "뮤턴트"; break;
+
+	}
+}
